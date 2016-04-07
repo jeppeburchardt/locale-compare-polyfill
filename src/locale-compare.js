ef393e970a2a908beb3983ad2aca65f29285047d
Jeppe Burchardt <jeppe@burchardt.com>
2016-04-07 23:24:15 +0200

    first locale compare

---
+++ src/locale-compare.js
@@ -0,0 +1,28 @@
+(function() {
+  'use strict';
+
+  var localeCompareSupport = 'ø'.localeCompare('p', 'da-DK') > 0;
+
+  if (!localeCompareSupport) {
+
+    var orders = {
+      'da': '  _-,;:!¡?¿.·\'"«»()[]{}§¶@*/\&#%`´^¯¨¸°©®+±÷×<=>¬|¦~¤¢$£¥01¹½¼2²3³¾456789AaªÁáÀàÂâÃãBbCcÇçDdÐðEeÉéÈèÊêËëFfGgHhIiÍíÌìÎîÏïJjKkLlMmNnÑñOoºÓóÒòÔôÕõPpQqRrSsßTtÞþUuÚúÙùÛûVvWwXxYyÝýÿÜüZzÆæÄäØøÖöÅåµ'
+    };
+
+    var original = String.prototype.localeCompare;
+
+    String.prototype.localeCompare = fuction(other, locale) {
+      if (!locale) { return original.apply(this, arguments); }
+      var lang = locale.split('-')[0];
+      var order = orders[lang];
+
+      var charA = null, charB = null, index = 0;
+      while (charA === charB && index < 100) {
+        charA = this.toString[index];
+        charB = other[index];
+        index++;
+      }
+      return Math.max(-1, Math.min(1, order.indexOf(charA) - order.indexOf(charB)));
+    }
+  }
+})();
---
+++ test/locale-compare.js
@@ -0,0 +1,15 @@
+// default
+//   ✓ should order b after a
+//   ✓ should order å before p
+//   ✓ should order uppercase before lowercase
+//   ✓ should order array
+//
+// localeCompare da-DK
+//   ✓ should order å after p in danish
+//   ✓ should order å after p in danish
+//   ✓ should order array
+//
+// localeCompare de-DE
+//   ✓ should order ß efter S
+//   ✓ should order ß before t
+//   ✓ should order array
